828bebcd46116dc25f93587a5a771de938d46082
yo6nam <yo6nam@gmail.com>
2018-11-19 04:27:01 +0200

    Added flash script

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+mv /opt/MMDVM/Config.h /opt/MMDVM/Config.bak
+cp -rf Config.h /opt/MMDVM
 cd /opt/MMDVM
 make -f Makefile.CMSIS
-
+rm -f /opt/MMDVM/Config.h
+read -p "Build should be done. Press any key to copy the firmware to /opt"
+mv /opt/MMDVM/Config.bak /opt/MMDVM/Config.h
+cp /opt/MMDVM/bin/mmdvm.hex /opt/mmdvm.hex
